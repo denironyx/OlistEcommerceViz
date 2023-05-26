b6a73a46f48ffc6f65ddcd740d115d9428095870
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2023-05-26 14:06:11 +0100

    restructure project folder

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -1,29 +1,37 @@
 # import libraries
 import pandas as pd
 import os
+import zipfile
 
+zip_path = 'data/raw/ecommerce.zip'
+extract_folder = 'data/extracted'
+processed_folder = 'data/processed/'
+
+# Extract the zip file
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_folder)
+
+data_files = os.listdir(extract_folder)
 
 # import data into python
-df = pd.read_csv('data/olist_geolocation_dataset.csv', index_col=False, delimiter=',')
-df = df.drop_duplicates(subset=['order_id'])
-df = df.where(pd.notnull(df), None)
-df = df.drop(['order_item_id'], axis=1)
+# df = pd.read_csv('data/olist_geolocation_dataset.csv', index_col=False, delimiter=',')
+# df = df.drop_duplicates(subset=['order_id'])
+# df = df.where(pd.notnull(df), None)
+# df = df.drop(['order_item_id'], axis=1)
 
 def ingest_data(data_file):
-    df = pd.read_csv(os.path.join(data_dir, data_file), index_col=False, delimiter=',')
+    df = pd.read_csv(os.path.join(extract_folder, data_file), index_col=False, delimiter=',')
     df = df.where(pd.notnull(df), None)
     return df
 
-data_dir = 'data'
-data_files = os.listdir(data_dir)
-
 for data_file in data_files:
-    modified_name = data_file.replace('olist_', '').replace('_dataset.csv', '')
-    # use a function to read the csvv and remove null.
-    modified_dataframe = ingest_data(data_file)
-    #create a seperate variable dynamically using globals() function
-    globals()[modified_name] = modified_dataframe
-    print(f"Processed file {data_file} and saved the modified DataFrame to {modified_name}")
+    if data_file.endswith('.csv'):
+        modified_name = data_file.replace('olist_', '').replace('_dataset.csv', '')
+        # use a function to read the csvv and remove null.
+        modified_dataframe = ingest_data(data_file)
+        #create a seperate variable dynamically using globals() function
+        globals()[modified_name] = modified_dataframe
+        print(f"Processed file {data_file} and saved the modified DataFrame to {modified_name}")
 
 ## Cleaning of the data
 
@@ -41,6 +49,9 @@ order_items.head()
 
 # payments
 payments = order_payments.copy()
+payments[payments['order_id'].duplicated()]
+payments[payments.order_id == '8e5148bee82a7e42c5f9ba76161dc51a']
+
 
 # product category
 product_category = product_category.copy()
@@ -53,8 +64,7 @@ products = products.drop_duplicates(subset=['product_id'])
 # join product information and category
 products = pd.merge(products, product_category, on='product_category_name', how='left')
 products.head()
-products.drop('product_category_name', axis=1, inplace=True)
-products.drop(['product_description_lenght', 'product_name_lenght', 'product_category_name'], axis=1, inplace=True)
+products.drop(['product_description_lenght', 'product_name_lenght', 'product_category_name', 'product_photos_qty', 'product_weight_g', 'product_length_cm', 'product_height_cm', 'product_width_cm'], axis=1, inplace=True)
 
 products = products.rename(columns={"product_category_name_english": "product_category_name"})
 
@@ -80,7 +90,7 @@ sellers = sellers.drop_duplicates(subset=['seller_id'])
 sellers.rename(columns={"seller_state": "state_code", "seller_city": "city", "seller_zip_code_prefix":"zip_code"}, inplace=True)
 
 # geolocation
-geolocation = pd.read_csv('data/olist_geolocation_dataset.csv', index_col=False, delimiter=',')
+geolocation = pd.read_csv(os.path.join(extract_folder, 'olist_geolocation_dataset.csv'), index_col=False, delimiter=',')
 geolocation = geolocation.drop(['geolocation_city', 'geolocation_state'], axis=1)
 geolocation = geolocation.drop_duplicates(subset=['geolocation_zip_code_prefix'], ignore_index=True)
 
@@ -113,7 +123,7 @@ orders.head()
 
 # state_code
 # state_codes = pd.read_csv('data/state_codes.csv', index_col=False, delimiter=',')
-state_codes = pd.read_excel('data/state_codes.xlsx', usecols=[0,1], names=['state_name','state_code'])
+state_codes = pd.read_excel(os.path.join(extract_folder, 'state_codes.xlsx'), usecols=[0,1], names=['state_name','state_code'])
 state_codes = state_codes[state_codes['state_code'].notnull()]
 state_codes.head()
 
@@ -124,3 +134,12 @@ sellers = pd.merge(sellers, geo_seller, on='zip_code', how='left')
 # Join state codes and seller or customer data
 customers = pd.merge(customers, state_codes, on='state_code', how='left')
 customers = pd.merge(customers, geo_customer, on='zip_code', how='left')
+
+# Export the data 
+customers.to_csv(f"{processed_folder}customers.csv", index=False)
+sellers.to_csv(f"{processed_folder}sellers.csv", index=False)
+reviews.to_csv(f"{processed_folder}reviews.csv", index=False)
+products.to_csv(f"{processed_folder}products.csv", index=False)
+order_items.to_csv(f"{processed_folder}order_items.csv", index=False)
+orders.to_csv(f"{processed_folder}orders.csv", index=False)
+payments.to_csv(f"{processed_folder}payments.csv", index=False)
